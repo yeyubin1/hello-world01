30f729a7d4871e079846706738b20c6981c80f58
yeyubin1 <1291484619@qq.com>
2021-01-12 20:57:00 +0800

    初始化代码

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+      <h2>预祝0713java全体学生前程似锦1</h2>
+      <h2>预祝0713java全体学生统统月薪过万</h2>
+      <h2>预祝0713java全体学生统统月薪过两万</h2>
+      <h2>预祝0713java全体学生统统月薪过三万</h2>
+      <h2>预祝0713java全体学生统统年薪过三十万</h2>
+   <img src="http://img12.360buyimg.com/n1/s450x450_jfs/t1/142295/6/19866/63874/5fe4408eE401889c8/8f5c2b37676a2264.jpg"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
