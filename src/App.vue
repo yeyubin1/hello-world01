6691cf2bfa5584b330cf05a2d9e53b243bf112eb
yeyubin1 <1291484619@qq.com>
2021-01-13 14:30:31 +0800

    自动部署test4

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
 
       <h2>叶喻斌是大帅哥</h2>
       <h2>芦启豪是大笨蛋</h2>
-   <img src="http://b107.photo.store.qq.com/psb?/V12ZIMcq0k0vdu/5T9VN8A1E8XB7S35AGEHRydjdpGJvWjn7hI2B9KC94I!/b/dGsAAAAAAAAA&bo=HwM4BAAAAAAFBwQ!&rf=viewer_4"/>
+   <img src="https://gmall-admin-file.oss-cn-shanghai.aliyuncs.com/2020-12-15/psb.jfif"/>
    <img src="https://gmall-admin-file.oss-cn-shanghai.aliyuncs.com/2020-12-15/IMG_3080.JPG"/>
   </div>
 </template>
